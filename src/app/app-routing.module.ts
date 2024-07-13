624c7b5a2657016e42fbea17252da265ec447ecc
pame-web <84988217+pame-web@users.noreply.github.com>
2024-07-13 20:12:06 +0000

    hasta navegacion de un grupo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,11 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'chat-grupal',
+    loadChildren: () => import('./chat-grupal/chat-grupal.module').then( m => m.ChatGrupalPageModule)
   },
   {
     path: 'grupo-uno',
